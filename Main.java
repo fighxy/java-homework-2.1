0eae15f2d2db9b785f2ca9762b1f4509d2ee2ee9
Ivan Kolotov <kolotov97@gmail.com>
2020-11-23 16:24:40 +0700

    1f-push

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int current_account = 2_000_000_000;
+        int transfer_amount = 500_000_000;
+        int total_value = current_account + transfer_amount;
+        System.out.println(total_value);
+    }
+}
+
